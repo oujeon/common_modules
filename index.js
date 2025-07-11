0f731d8f03883e2329f110ec85e3bbea5b101f88
oujeon <oujeon@gmail.com>
2025-07-11 19:49:42 +0900

    날짜함수

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 "use strict";
 
 import { sample } from "./sample";
+import {
+  y_m_d,
+  y_m_d_h_m_s,
+  y_m_d_h_m_s_i,
+  ymd,
+  ymdhms,
+  ymdhmsi,
+  today_y_m_d,
+  today_y_m_d_h_m_s,
+  today_y_m_d_h_m_s_ms,
+  today_ymd,
+  today_ymdhms,
+  today_ymdhmsms,
+} from "./fn_date";
 
-export { sample };
+export {
+  sample,
+  y_m_d,
+  y_m_d_h_m_s,
+  y_m_d_h_m_s_i,
+  ymd,
+  ymdhms,
+  ymdhmsi,
+  today_y_m_d,
+  today_y_m_d_h_m_s,
+  today_y_m_d_h_m_s_ms,
+  today_ymd,
+  today_ymdhms,
+  today_ymdhmsms,
+};
